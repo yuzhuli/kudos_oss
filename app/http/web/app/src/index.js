1123ed9d54e538946a2b22315efafc06d9b88f41
Yuzhu Li <liyuzhumail@gmail.com>
2019-10-24 16:13:56 -0700

    add react app

---
+++ app/http/web/app/src/index.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+import Main from './Main';
+
+const history = createBrowserHistory();
+
+ReactDOM.render((
+  <Router history={history}>
+    <Main history={history} />
+  </Router>
+), document.getElementById('root'))
